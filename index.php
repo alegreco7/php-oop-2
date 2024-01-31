64f0fceeccc6d65b6eca8a365394e54344c1324e
alegreco7 <alexgreco2002@gmail.com>
2024-01-31 15:57:08 +0100

    definizione della classe categoria

--- index.php
+++ index.php
@@ -25,4 +25,23 @@ class Prodotto {
     }
 }
 
+// Definizione della classe Categoria
+class Categoria {
+    private $nome;
+    private $icona;
+
+    public function __construct($nome, $icona) {
+        $this->nome = $nome;
+        $this->icona = $icona;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getIcona() {
+        return $this->icona;
+    }
+}
+
 ?>
